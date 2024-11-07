f1eb2375e0be6491f33b8cbfd701077e273524eb
Shashank Shukla <shashankshukla063@gmail.com>
2024-11-07 05:13:15 -0800

    this is custom react

---
+++ custom react/app.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading" }, "i am shashank shukla"),
+    React.createElement("h1", { id: "heading" }, "i am h1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "i am shashank shukla"),
+    React.createElement("h1", { id: "heading" }, "i am h1"),
+  ]),
+]); // this is nested element using react{}
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "my name is shashank shukla"
+);
+console.log(parent); //returns a object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
---
+++ custom react/readme.md
@@ -0,0 +1,16 @@
+# react
+
+# Parcel
+
+- dev Build
+- local Server
+- HMR = Hot Module replacement
+- File watching Algorithm - Written in c++
+- Cashing = fast build
+- image optimization
+- Minification
+- bunduling
+- compressing
+- consitent hashing
+- code spitting
+- differential bundling - support older browser
