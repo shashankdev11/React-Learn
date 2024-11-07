9082661b7d460b1c43af7d9c86af855a1103e004
Shashank Shukla <shashankshukla063@gmail.com>
2024-11-07 11:37:32 -0800

    react jsx element

--- custom react/app.js
+++ custom react/app.js
@@ -1,24 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading" }, "i am shashank shukla"),
-    React.createElement("h1", { id: "heading" }, "i am h1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "i am shashank shukla"),
-    React.createElement("h1", { id: "heading" }, "i am h1"),
-  ]),
-]); // this is nested element using react{}
+//React.createelement => js object => htmlelement(render)
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "my name is shashank shukla"
-);
-console.log(parent); //returns a object
+const heading = React.createElement("h1", { id: "heading" }, "this is shashank shukla");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading)
+
+
+//jsx (trsnspiled befor it reaches the js) - Parcel -babel = it is a package which convert hte code that is react can understand
+
+//jsx => React.createelement => js object => htmlelement(render){through babel}
+
+const jsxheading = <h1 id="heading" className="head">my name  is shashank shukla</h1>
+
+const jsxroot = ReactDOM.createRoot(document.getElementById("root"));
+
+jsxroot.render(jsxheading)
